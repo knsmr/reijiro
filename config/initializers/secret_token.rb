928b7c809237c77cc0551a0d4322bf99eacdf512
Ken Nishimura <ken97531@gmail.com>
2012-02-06 13:38:46 +0900

    initial commit with twitter bootstrap

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Reijiro::Application.config.secret_token = '836556448f263f2b3f7943bcdb31372123b0d3d198375a49d10b696a064027f49d5d7cae840ac4ae2dcd4962d83807c5825d8a64610f04046e090ac439dfae08'
