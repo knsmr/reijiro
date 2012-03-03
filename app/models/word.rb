7d99269fec66de0cd25de49b0ff5201d5521ca8c
Ken Nishimura <ken97531@gmail.com>
2012-03-03 09:02:39 +0900

    use own dictionary tables instead of dbfile outside the app

--- app/models/word.rb
+++ app/models/word.rb
@@ -6,18 +6,22 @@ class Word < ActiveRecord::Base
   scope :unclipped, where('id not in (select word_id from words inner join clips on words.id = clips.word_id)')
 
   class << self
+    def lookup(word)
+      word.downcase!
+      items = Item.where(entry: word).all
+      items += Invert.where(token: word).map(&:item)
+      items.uniq.map(&:body).join("\n")
+    end
+
     def find_or_lookup(word)
       word.downcase!
       if w = Word.find_by_entry(word)
         w
       else
-        definition = Eijiro.lookup(word)
+        definition = lookup(word)
         level = Eijiro.level(word)
         unless definition.empty?
-          Word.create(entry: word,
-                      level: level,
-                      definition: definition.join("\n"))
-
+          Word.create(entry: word, level: level, definition: definition)
         else
           nil
         end
