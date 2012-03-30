3f0e729124343fb29d309e8618d898a969c95df5
Ken Nishimura <ken97531@gmail.com>
2012-03-31 08:49:54 +0900

    add basic authentication config

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,7 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+  if Reijiro::Application.config.respond_to?(:basic_auth)
+    user, pass = Reijiro::Application.config.basic_auth
+    http_basic_authenticate_with name: user, password: pass
+  end
 end
