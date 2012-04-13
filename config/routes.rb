c01186f9f44774f747713148c065b08929dc920e
Ken Nishimura <ken97531@gmail.com>
2012-04-14 08:23:16 +0900

    remove unused route

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,6 @@ Reijiro::Application.routes.draw do
 
   match '/search' => 'words#search', as: 'search'
   match '/import(/:level)' => 'words#import_from_alc12000', as: 'importalc'
-  match '/nextup' => 'clips#nextup', as: 'nextup'
   match '/next' => 'clips#next', as: 'next'
   match '/stats' => 'clips#stats', as: 'stats'
   root to: 'clips#index'
