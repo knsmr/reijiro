901b613f843755c50983f4cd4129ea1b00911962
Ken Nishimura <ken97531@gmail.com>
2012-07-01 12:51:02 +0900

    remove unnecessary files

--- lib/tasks/test_clips.rb
+++
@@ -1,9 +0,0 @@
-namespace :test do
-  task create_clips: :environment do
-    Word.unclipped.each do |w|
-      Clip.create(word_id: w.id, status: 0) if rand > 0.2
-      puts "created #{w.entry}"
-    end
-  end
-end
-
