b4ab841c1a7657987d78326c11a9927ba15509dd
Ken Nishimura <ken97531@gmail.com>
2012-07-08 06:03:48 +0900

    fix: when no words remaining in a level, say so

--- app/controllers/levels_controller.rb
+++ app/controllers/levels_controller.rb
@@ -1,5 +1,9 @@
 class LevelsController < ApplicationController
   def index
+    @to_import = []
+    (1..12).each do |l|
+      @to_import[l] = Level.yet_to_import(l, 5)
+    end
   end
 
   def show
