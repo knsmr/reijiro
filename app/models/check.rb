04983d43afbb12997ac04daa9241b51b0ec65e60
Ken Nishimura <ken97531@gmail.com>
2012-10-26 10:46:25 +0900

    show the number of clips you checked within that day

--- app/models/check.rb
+++ app/models/check.rb
@@ -2,6 +2,8 @@ class Check < ActiveRecord::Base
   belongs_to :word
   attr_accessible :newstat, :oldstat, :word_id
 
+  scope :today, lambda{ where('created_at > ?', Time.now.beginning_of_day + 3.hours) }
+
   class << self
     def check_months
       checks = Check.find(:all, :order => 'created_at')
